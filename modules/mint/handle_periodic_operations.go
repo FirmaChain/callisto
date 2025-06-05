c3b2976b6a2f4b6304d04ef0ee5e7fb760c6c4f6
Luca Palla <l.palla1995@gmail.com>
2025-06-05 17:42:41 +0200

    Remove periodic inflation update due to a bug

--- modules/mint/handle_periodic_operations.go
+++ modules/mint/handle_periodic_operations.go
@@ -1,25 +1,23 @@
 package mint
 
 import (
-	"github.com/forbole/callisto/v4/modules/utils"
-
-	"github.com/go-co-op/gocron"
 	"github.com/rs/zerolog/log"
 )
 
 // RegisterPeriodicOperations implements modules.PeriodicOperationsModule
-func (m *Module) RegisterPeriodicOperations(scheduler *gocron.Scheduler) error {
+// Disabled due to a bug in the Cosmos SDK protobuf.
+/*func (m *Module) RegisterPeriodicOperations(scheduler *gocron.Scheduler) error {
 	log.Debug().Str("module", "mint").Msg("setting up periodic tasks")
 
 	// Setup a cron job to run every midnight
-	if _, err := scheduler.Every(1).Day().At("00:00").Do(func() {
+	if _, err := scheduler.Every(1).Minute().Do(func() {
 		utils.WatchMethod(m.UpdateInflation)
 	}); err != nil {
 		return err
 	}
 
 	return nil
-}
+}*/
 
 // updateInflation fetches from the REST APIs the latest value for the
 // inflation, and saves it inside the database.
